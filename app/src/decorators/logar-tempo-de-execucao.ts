e36845aa4f0bd57ae53f3a11f014ede4e908d7a4
Michel Ferreira <michel.ferreira.melo@gmail.com>
2023-03-21 15:04:46 -0300

    feat: API do Decorator

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,3 +1,20 @@
 export function logarTempoDeExecucao() {
-  
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    return descriptor;
+  }
 }
+
+/* 
+  target: any
+    Este é o objeto que contém o método decorado. Em outras palavras, é a classe à qual o método pertence.
+
+  propertyKey: string
+    Este é o nome do método decorado.
+
+  descriptor: PropertyDescriptor
+    Este é um objeto que contém a descrição do método decorado, incluindo informações como se o método é uma propriedade, se é um método estático ou de instância, e assim por diante.
+*/
